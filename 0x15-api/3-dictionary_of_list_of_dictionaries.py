63ad922ecfaad3d9bac83b756c77ba037bdc5d81
myrajarenga <myrajarenga1234@gmail.com>
2023-04-24 14:10:41 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
-"""script to send get request to a url and extract data"""
+"""Script to fetch todos of all employees from an\
+API and export to JSON format"""
+import json
+import requests
 
-if __name__ == '__main__':
-    import json
-    import requests
-    from sys import argv
 
-    employee_id = argv[1]
-    total_todos = 0
-    done_todos = 0
-    done_todo_titles = []
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    users = requests.get(url).json()
+    todo_dict = {}
 
-    result = requests.get('https://jsonplaceholder.typicode\
-.com/users/' + employee_id)
-    employee_name = result.json().get('name')
+    for user in users:
+        user_id = str(user.get("id"))
+        username = user.get("username")
+        todos = requests.get(f"{url}{user_id}/todos").json()
+        todo_list = []
+        for todo in todos:
+            todo_dict_item = {
+                "username": username,
+                "task": todo.get("title"),
+                "completed": todo.get("completed")
+            }
+            todo_list.append(todo_dict_item)
+        todo_dict[user_id] = todo_list
 
-    result = requests.get('https://jsonplaceholder.typicode\
-.com/users/' + employee_id + '/todos')
-    employee_todos = result.json()
-
-    for item in employee_todos:
-        total_todos += 1
-        if item.get('completed') is True:
-            done_todos += 1
-            done_todo_titles.append(item.get('title'))
-
-    print('Employee {} is done with tasks({}/{}):'.format(
-        employee_name,
-        done_todos,
-        total_todos
-    ))
-
-    todo_dict = {
-        'employee_name': employee_name,
-        'total_todos': total_todos,
-        'done_todos': done_todos,
-        'done_todo_titles': done_todo_titles
-    }
-
-    with open('todo_all_employees.json', 'w') as f:
+    with open("todo_all_employees.json", "w") as f:
         json.dump(todo_dict, f)
