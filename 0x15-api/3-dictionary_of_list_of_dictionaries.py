b16d880dbce6089f0f2aa51b43a240bead7b6fc8
myrajarenga <myrajarenga1234@gmail.com>
2023-04-24 14:57:24 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""script to fetch todos of all employees from an \
+"""script to fetch todos of all employees from an\
 API and export to JSON format.
-The script fetches todos of all users from the JSONPlaceholder\
- API and exports
-them to a JSON file in the formatooou
-ER_ID": [
+The script fetches todos of all users from the JSONPlaceholder API and exports
+them to a JSON file in the format:
+
+{
+    "USER_ID": [
         {
             "username": "USERNAME",
             "task": "TASK_TITLE",
@@ -15,22 +16,31 @@ ER_ID": [
     ...
 }
 
-ON file is saved with the name "todo_all_employees.json".
+The JSON file is saved with the name "todo_all_employees.json".
 """
+
 import json
 import requests
 
 
 if __name__ == "__main__":
+    # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/users/"
+
+    # Fetch all users from the API
     users = requests.get(url).json()
+
+    # Dictionary to store todos of all users
     todo_dict = {}
 
+    # Loop through each user and fetch their todos
     for user in users:
         user_id = str(user.get("id"))
         username = user.get("username")
         todos = requests.get(f"{url}{user_id}/todos").json()
         todo_list = []
+
+        # Loop through each todo of the user and store it in a dictionary
         for todo in todos:
             todo_dict_item = {
                 "username": username,
@@ -38,7 +48,10 @@ if __name__ == "__main__":
                 "completed": todo.get("completed")
             }
             todo_list.append(todo_dict_item)
+
+        # Store the list of todos of the user in the todo_dict
         todo_dict[user_id] = todo_list
 
+    # Write the todo_dict to a JSON file
     with open("todo_all_employees.json", "w") as f:
         json.dump(todo_dict, f)
