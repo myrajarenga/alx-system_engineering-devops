f6605eb03db36279775fe595a1f9f21a815cf134
myrajarenga <myrajarenga>
2023-05-11 07:02:25 +0000

    puppet script that replace "phpp" with "php"

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #puppet script that replaces the "phpp" with "php" in the /var/www/html/wp-settings.php file to prevent 500 server error
-file_pth = '/var/www/html/wp-settings.php'
-exec{{ 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_path}",
+$file_pth = '/var/www/html/wp-settings.php'
+exec{ 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_pth}",
   path    => ['/bin','/usr/bin']
 }
