3312a27ec592e417d4eb731ad6b971cb7c19840b
root <myrajarenga1234@gmail.com>
2023-05-19 14:30:34 +0000

    web stack debugging using apache bench marking

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,24 +1,11 @@
-# acript increases the amount of traffic an Nginx server can handle
-class nginx::config {
-  file { '/etc/default/nginx':
-    ensure  => present,
-    content => "# Managed by Puppet\nULIMIT=\"-n 4096\"\n",
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
+# acript increases the amount of traffic an Nginx server can handle}
+exec { 'fixing nginx-conf':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
 
-  exec { 'fix_for_nginx':
-    command     => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-    path        => '/usr/local/bin/:/bin/',
-    refreshonly => true,
-    subscribe   => File['/etc/default/nginx'],
-  }
-
-  exec { 'nginx-restart':
-    command     => '/bin/systemctl restart nginx',
-    path        => '/usr/local/bin/:/bin/',
-    refreshonly => true,
-    subscribe   => Exec['fix_for_nginx'],
-  }
+# Restarting Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
